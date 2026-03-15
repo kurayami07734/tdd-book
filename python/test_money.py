14a7fd974302824898a665e8efd638886a0ce696
Aditya Ghidora <adityaghidora@gmail.com>
2026-03-15 22:41:41 +0530

    feat: add support for adding same currencies

--- python/test_money.py
+++ python/test_money.py
@@ -21,6 +21,20 @@ class Money:
         return Money(self.amount / divisor, self.currency)
 
 
+class Portfolio:
+    moneys: list[Money]
+
+    def __init__(self) -> None:
+        self.moneys = []
+
+    def add(self, *moneys: Money) -> None:
+        self.moneys.extend(moneys)
+
+    def evaluate(self, currency: str) -> Money:
+        total = sum(m.amount for m in self.moneys if m.currency == currency)
+        return Money(total, currency)
+
+
 class TestMoney(unittest.TestCase):
     def testMultiplication(self):
         fiver = Money(5, "USD")
@@ -39,6 +53,16 @@ class TestMoney(unittest.TestCase):
         quarter = Money(1000.5, "KRW")
 
         self.assertEqual(og.divide(4), quarter)
+
+    def testAddition(self):
+        five = Money(5, "USD")
+        ten = Money(10, "USD")
+        fifteen = Money(15, "USD")
+
+        portfolio = Portfolio()
+        portfolio.add(five, ten)
+
+        self.assertEqual(portfolio.evaluate("USD"), fifteen)
 
 
 if __name__ == "__main__":
