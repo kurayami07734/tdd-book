94573cdf9083151281ed1de8f419f4084ab3b540
Aditya Ghidora <adityaghidora@gmail.com>
2026-03-15 22:13:30 +0530

    feat: add multi currency support

--- js/test_money.js
+++ js/test_money.js
@@ -1,17 +1,25 @@
 import assert from "assert";
 
-class Dollar {
-  constructor(amount) {
+class Money {
+  constructor(amount, currency) {
     this.amount = amount;
+    this.currency = currency;
   }
 
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Money(this.amount * multiplier, this.currency);
   }
 }
 
-const fiver = new Dollar(5);
+const fiver = new Money(5, "USD");
 
 const tenner = fiver.times(2);
 
 assert.strictEqual(tenner.amount, 10);
+assert.strictEqual(tenner.currency, "USD");
+
+const tenEuro = new Money(10, "EUR");
+const twentyEuro = tenEuro.times(2);
+
+assert.strictEqual(twentyEuro.amount, 20);
+assert.strictEqual(twentyEuro.currency, "EUR");
--- python/test_money.py
+++ python/test_money.py
@@ -1,20 +1,29 @@
 import unittest
 
 
-class Dollar:
-    def __init__(self, amount: int) -> None:
+class Money:
+    def __init__(self, amount: int, currency: str) -> None:
         self.amount = amount
+        self.currency = currency
 
-    def times(self, multiplier: int) -> "Dollar":
-        return Dollar(self.amount * multiplier)
+    def times(self, multiplier: int) -> "Money":
+        return Money(self.amount * multiplier, self.currency)
 
 
 class TestMoney(unittest.TestCase):
     def testMultiplication(self):
-        fiver = Dollar(5)
+        fiver = Money(5, "USD")
         tenner = fiver.times(2)
 
         self.assertEqual(tenner.amount, 10)
+        self.assertEqual(tenner.currency, "USD")
+
+    def testMultiplicationEUR(self):
+        fiver = Money(10, "EUR")
+        tenner = fiver.times(2)
+
+        self.assertEqual(tenner.amount, 20)
+        self.assertEqual(tenner.currency, "EUR")
 
 
 if __name__ == "__main__":
