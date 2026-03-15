fa78fcf7d74deb0e689d74f988be3f7eb04164f0
Aditya Ghidora <adityaghidora@gmail.com>
2026-03-15 22:06:10 +0530

    feat: first green test in js

---
+++ js/test_money.js
@@ -0,0 +1,17 @@
+import assert from "assert";
+
+class Dollar {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  times(multiplier) {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+const fiver = new Dollar(5);
+
+const tenner = fiver.times(2);
+
+assert.strictEqual(tenner.amount, 10);
